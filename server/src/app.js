6c767c5ad9421429d5610b0f24a2a427cc30631a
sazzadshakhbd <sazzadshakhbd@gmail.com>
2025-04-09 08:30:50 +0600

    How to secure API using xss-clean and express-rate-limit

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,24 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const createError = require('http-errors');
+const xssClean = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 5, // limit each IP to 5 requests per windowMs
+  message: 'Too many requests, please try again later.',
+})
+
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(xssClean());
+app.use(rateLimiter);
+
+
+
+
 
 app.get('/test', (req, res) => {
   res.status(200).send({
